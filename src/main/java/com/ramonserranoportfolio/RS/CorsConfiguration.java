dd5bf9baf81aef6dba717b65399ac89ac356ac92
Ramón Serrano <ramonserrano76@gmail.com>
2023-03-12 22:48:45 -0300

    first commit

---
+++ src/main/java/com/ramonserranoportfolio/RS/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.ramonserranoportfolio.RS;
+
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/**
+* Clase que habilita CORS
+* @author YOProgramo
+*/
+
+//    @EnableWebMvc
+//    @Configuration
+//    public class CorsConfiguration implements WebMvcConfigurer {      
+//        @Override
+//        public void addCorsMappings(CorsRegistry registry){
+//            registry.addMapping("/**")
+//                .allowedOrigins("*")          
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*");
+//        }
+//    }    
---
+++ src/main/java/com/ramonserranoportfolio/RS/Repository/Rhys.java
@@ -0,0 +1,14 @@
+package com.ramonserranoportfolio.RS.Repository;
+
+import com.ramonserranoportfolio.RS.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Usuario
+ */
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
